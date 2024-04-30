cab11a36967729f9a8855132d70822fce81d61af
urielbang <uri32655@gmail.com>
2024-04-30 09:55:54 +0300

    server

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.use("/api/v1/contact", userRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`connet to port ${PORT}`);
 });
