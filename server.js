e21a88757f68645f073257b95e1609e71435bdef
urielbang <uri32655@gmail.com>
2024-04-30 09:58:07 +0300

    port

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./app");
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`connet to port ${PORT}`);
+  console.log(`connet to port ${port}`);
 });
